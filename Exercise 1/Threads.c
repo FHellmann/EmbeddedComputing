16da02cf5d797c6fdecc005e0c55dd699aaf8f11
Fabio Hellmann <fhellmann93@googlemail.com>
2016-10-20 09:39:33 +0200

    Add comments

--- Exercise 1/Threads.c
+++ Exercise 1/Threads.c
@@ -4,29 +4,34 @@
 
 void* function( void )
 {
+	// Get the thread id of the current execution thread
 	pthread_t *id = pthread_self();
+	// Let the current thread sleep for a random time span
 	sleep( rand() % 50);
+	// Print out the current executing thread id
 	printf( "This is thread %d\n", id );
+	// Return the thread id when exiting this thread
 	pthread_exit((void*)id);
 }
 
 int main( void )
 {
+	// Initialise the thread attributes
 	pthread_attr_t attr;
-
 	pthread_attr_init( &attr );
 	pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_JOINABLE );
-	// Wenn DETACHTED anstatt JOINABLE verwendet wird, kann hinterher die Funktion pthread_join nicht verwendet werden.
 
+	// Create 2 threads
 	pthread_t th1, th2;
-
 	pthread_create( &th1, &attr, &function, NULL );
 	pthread_create( &th2, &attr, &function, NULL );
 
+	// Wait for the 2 started threads to exit
 	pthread_t *result1, *result2;
 	pthread_join( th1, (void**)&result1 );
 	pthread_join( th2, (void**)&result2 );
 
+	// Compare the thread id before starting the thread with the thread id after exiting the thread
 	if( th1 == result1 && th2 == result2 ) {
 		printf( "Thread 1 has the same id before %d and after %d\n", th1, result1 );
 		printf( "Thread 2 has the same id before %d and after %d\n", th2, result2 );
