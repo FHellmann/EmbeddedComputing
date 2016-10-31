6da0f9d537abe70146ea105e1254edecc50540f8
ifw13077 <ifw13077@pn.cs.hm.edu>
2016-10-31 12:37:58 +0100

    Create dynamic timerate program

--- Exercise 2/DynamicTimerate.c
+++ Exercise 2/DynamicTimerate.c
@@ -1,27 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <string.h>
 
-int main( int argc, char *argv[] )
-{
+void changeSystemTick(unsigned int microsecs) {
 	struct timespec frequency, requestStart, requestEnd;
 	frequency.tv_sec = 0;
-	while(true) {
+	
+	while(1) {
 		// Get the difference of the start and end time from the last iteration
-		long diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
+		long diff = requestEnd.tv_nsec - requestStart.tv_nsec - microsecs;
 		// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
 		
 		// Get time when starting loop
 		clock_gettime(CLOCK_REALTIME, &requestStart);
+		printf("Time taken (nanoseconds): %ld\n", requestStart);
 		// Sample: start = 10
 		
-		frequency.tv_nsec = 1000 - diff;
+		frequency.tv_nsec = microsecs - diff;
 		clock_nanosleep(CLOCK_REALTIME, 0, &frequency, NULL);
 		
 		// Get time when loop ends
 		clock_gettime(CLOCK_REALTIME, &requestEnd);
+		printf("Time taken (nanoseconds): %ld\n", requestEnd);
 		// Sample: end = 1050
 	}
+}
+
+int main( int argc, char *argv[] )
+{
+	int sleepTime = strtol(argv[0], NULL, 10);
+	
+	changeSystemTick(sleepTime);
 
 	return EXIT_SUCCESS;
 }
--- Exercise 2/FixTimerate.c
+++ Exercise 2/FixTimerate.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 int main( void )
 {
 	struct timespec frequency, requestStart, requestEnd;
 	frequency.tv_sec = 0;
-	while(true) {
+	while(1) {
 		// Get the difference of the start and end time from the last iteration
 		long diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
 		// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
