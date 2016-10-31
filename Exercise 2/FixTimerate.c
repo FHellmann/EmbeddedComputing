423e566b1a08b22afbe966088f92c5133f2151e9
ifw13077 <ifw13077@pn.cs.hm.edu>
2016-10-31 13:03:22 +0100

    next try to fix

--- Exercise 2/FixTimerate.c
+++ Exercise 2/FixTimerate.c
@@ -7,21 +7,19 @@ int main( void )
 	struct timespec frequency, requestStart, requestEnd;
 	frequency.tv_sec = 0;
 	long diff = 0;
+	long sleepTime = 1000000;
 	while(1) {
 		// Get time when starting loop
-		clock_gettime(CLOCK_REALTIME, &requestStart);
-		// Sample: start = 10
+		clock_gettime(CLOCK_MONOTONIC, &requestStart);
 		
-		frequency.tv_nsec = 1000 - diff;
-		clock_nanosleep(CLOCK_REALTIME, 0, &frequency, NULL);
+		frequency.tv_nsec = sleepTime - diff;
+		clock_nanosleep(CLOCK_MONOTONIC, 0, &frequency, NULL);
 		
 		// Get time when loop ends
-		clock_gettime(CLOCK_REALTIME, &requestEnd);
-		// Sample: end = 1050
+		clock_gettime(CLOCK_MONOTONIC, &requestEnd);
 		
 		// Get the difference of the start and end time from the last iteration
-		diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
-		// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
+		diff = requestEnd.tv_nsec - requestStart.tv_nsec - sleepTime;
 	}
 
 	return EXIT_SUCCESS;
