a535e9b3285562f6decfa59024e5b914b58a7eac
ifw13077 <ifw13077@pn.cs.hm.edu>
2016-10-31 13:36:35 +0100

    test for bugfix

--- Exercise 2/FixTimerate.c
+++ Exercise 2/FixTimerate.c
@@ -4,29 +4,15 @@
 
 int main( void )
 {
-	struct timespec start, end, loop;
+	struct timespec loop;
 	
-	const int NSEC_IN_SEC = 10000000001, INTERVAL = 1000001;
+	const int INTERVAL = 1000000;
 	
 	loop.tv_nsec = INTERVAL;
 	loop.tv_sec = 0;
 	
 	while(1) {
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		if(start.tv_nsec >= NSEC_IN_SEC) {
-			start.tv_nsec -= NSEC_IN_SEC;
-			start.tv_sec = 0;
-		}
-		
 		clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &loop, NULL);
-		
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		if(end.tv_nsec >= NSEC_IN_SEC) {
-			end.tv_nsec -= NSEC_IN_SEC;
-			end.tv_sec = 0;
-		}
-		
-		loop.tv_nsec = INTERVAL - (end.tv_nsec - start.tv_nsec - INTERVAL);
 	}
 
 	return EXIT_SUCCESS;
