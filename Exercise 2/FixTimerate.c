4980e6a2008985867b8151b85a5f5c32e6e8bcfa
ifw13077 <ifw13077@pn.cs.hm.edu>
2016-10-31 12:24:56 +0100

    Add programs for exercise 2

---
+++ Exercise 2/FixTimerate.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int main( void )
+{
+	struct timespec frequency, requestStart, requestEnd;
+	frequency.tv_sec = 0;
+	while(true) {
+		// Get the difference of the start and end time from the last iteration
+		long diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
+		// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
+		
+		// Get time when starting loop
+		clock_gettime(CLOCK_REALTIME, &requestStart);
+		// Sample: start = 10
+		
+		frequency.tv_nsec = 1000 - diff;
+		clock_nanosleep(CLOCK_REALTIME, 0, &frequency, NULL);
+		
+		// Get time when loop ends
+		clock_gettime(CLOCK_REALTIME, &requestEnd);
+		// Sample: end = 1050
+	}
+
+	return EXIT_SUCCESS;
+}
