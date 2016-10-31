1cf1c74b70dc26a4a569d625b46daa24d8671ff8
ifw13077 <ifw13077@pn.cs.hm.edu>
2016-10-31 12:52:34 +0100

    Possible fix

--- Exercise 2/DynamicTimerate.c
+++ Exercise 2/DynamicTimerate.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 
 void changeSystemTick(unsigned int microsecs) {
 	struct timespec frequency, requestStart, requestEnd;
--- Exercise 2/FixTimerate.c
+++ Exercise 2/FixTimerate.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 int main( void )
 {
 	struct timespec frequency, requestStart, requestEnd;
 	frequency.tv_sec = 0;
+	long diff = 0;
 	while(1) {
-		// Get the difference of the start and end time from the last iteration
-		long diff = 0;
-		if(requestEnd.tv_nsec > 0) {
-			diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
-			// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
-		}
-		
 		// Get time when starting loop
 		clock_gettime(CLOCK_REALTIME, &requestStart);
 		// Sample: start = 10
@@ -23,6 +18,10 @@ int main( void )
 		// Get time when loop ends
 		clock_gettime(CLOCK_REALTIME, &requestEnd);
 		// Sample: end = 1050
+		
+		// Get the difference of the start and end time from the last iteration
+		diff = requestEnd.tv_nsec - requestStart.tv_nsec - 1000;
+		// Sample: diff = end - start - 1000 = 1050 - 10 - 1000 = 40
 	}
 
 	return EXIT_SUCCESS;
